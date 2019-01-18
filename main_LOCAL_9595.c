2fb441b46ded7ad2bab97657d8a9585ed818f679
devops-intellipaat <hemant@intellipaat.com>
2019-01-18 02:33:59 +0000

    added feature2

--- main_LOCAL_9595.c
+++ main_LOCAL_9595.c
@@ -5,14 +5,11 @@ Initial Function()
   //Sample Code
 }
 
-<<<<<<< HEAD
 Initial Security()
 {
  //Sample Code
 }
 
-=======
->>>>>>> added feature2
 feaature2()
 {
   //Sample Code for Feature 2
--- main_REMOTE_9595.c
+++ main_REMOTE_9595.c
@@ -5,14 +5,6 @@ Initial Function()
   //Sample Code
 }
 
-<<<<<<< HEAD
-Initial Security()
-{
- //Sample Code
-}
-
-=======
->>>>>>> added feature2
 feaature2()
 {
   //Sample Code for Feature 2
